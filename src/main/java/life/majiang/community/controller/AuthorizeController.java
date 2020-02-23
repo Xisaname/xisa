1af277629864ef46e894eb9474a441899dcca5cd
范新东 <1287100643@qq.com>
2020-02-23 14:37:13 +0800

    new file repostory

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -32,7 +31,6 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                             @RequestParam(name="state") String state,
-                           HttpServletRequest request,
                            HttpServletResponse response
                            ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
