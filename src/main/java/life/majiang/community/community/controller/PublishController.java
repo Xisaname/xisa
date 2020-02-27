73eefbae1374027efcea2113e44bd5c54e67075b
范新东 <1287100643@qq.com>
2020-02-27 13:07:41 +0800

    2020/2/27 question publish

--- src/main/java/life/majiang/community/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/community/controller/PublishController.java
@@ -1,12 +1,81 @@
 package life.majiang.community.community.controller;
 
+import life.majiang.community.community.mapper.QuestionMapper;
+import life.majiang.community.community.mapper.UserMapper;
+import life.majiang.community.community.model.Question;
+import life.majiang.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+    @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+    @RequestParam(value = "title",required = false) String title,
+    @RequestParam(value = "description",required = false) String description,
+    @RequestParam(value = "tag",required = false) String tag,
+    HttpServletRequest request,
+    Model model
+            ){
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+        if(title == null || title == ""){
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+        if(description == null || description == ""){
+            model.addAttribute("error","问题补充不能为空");
+            return "publish";
+        }
+        if(tag == null || tag == ""){
+            model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
+
+        Cookie[] cookies = request.getCookies();
+        User user=null;
+        for (Cookie cookie: cookies
+        ) {
+            if(cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                user=userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        if(user==null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+        Question question = new Question();
+        question.setTitle(title);
+        question.setTag(tag);
+        question.setDescription(description);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+
+        questionMapper.create(question);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/life/majiang/community/community/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package life.majiang.community.community.mapper;
+
+import life.majiang.community.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create (Question question);
+}
